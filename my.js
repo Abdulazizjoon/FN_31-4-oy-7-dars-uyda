f674cb6641435d596f7d852ec78bea944594ce88
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-11-08 21:24:53 +0500

    .

---
+++ my.js
@@ -0,0 +1,139 @@
+//1-masala
+// let arr = [1, 2, 4, 8, 9, 5]
+// function maxElement(a) {
+//     let sum=0
+//     for (let i = 0; i < a.length; i++) {
+//              sum+=a[i]
+//     }
+//     return sum
+// }
+// let res = maxElement(arr)
+// console.log(res);
+
+//2-masala
+// let arr = [1, 2, 4, 8, 9, 5]
+// function minElement(a) {
+//     let sum=0
+//     for (let i = 0; i < a.length; i++) {
+//         if (sum < a[i]) {
+//             sum=a[i]
+//         }
+//     }
+//     return sum
+// }
+// let res=minElement(arr)
+// console.log(res);
+
+//3-masala
+// let arr = [1, 2, 4, 8, 9, 5];
+// function minElement(a) {
+//   let sum = a[0];
+//   for (let i = 1; i < a.length; i++) {
+//     if (a[i]<sum) {
+//       sum = a[i];
+//     }
+//   }
+//   return sum;
+// }
+// let res = minElement(arr);
+// console.log(res);
+
+//4-masaala
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function juft(a) {
+//   for (let i = 0; i < a.length; i++) {
+//     let newarr = [];
+//     if (a[i] % 2 == 0) {
+//       newarr = a[i];
+//       console.log(newarr);
+//     }
+//   }
+// }
+// let res = juft(arr);
+
+//5-masala
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let newarr = []
+// function toqElement(a) {
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i]%2==1) {
+//             newarr = a[i]
+//             console.log(newarr);
+//         }
+//     }
+// }
+// toqElement(arr)
+
+//6-masala
+// let arr = [1, -2, 3, 4, 5, -6, 7, 8, 9, 10];
+// function toqElement(a) {
+//     let sum = 0;
+//     let tek=0
+//   for (let i = 0; i < a.length; i++) {
+//     if (a[i] > 0) {
+//         sum += a[i];
+//         tek+=1
+//     }
+//   }
+//   return sum / tek;
+// }
+// let res = toqElement(arr);
+// console.log(res);
+
+//7-masala
+// let arr = [1, -2, 3, -4, 5, -6, 7, -8, -9, 10];
+// function manfiyNumRes(a) {
+//   let sum = 0;
+//   for (let i = 0; i < a.length; i++) {
+//     if (a[i] < 0) {
+//       sum += a[i];
+//     }
+//   }
+//   return sum;
+// }
+// let res = manfiyNumRes(arr);
+// console.log(res);
+
+
+//9-masala     HATO OHIRIGACHA ISHLAY OLMADIM
+// let arr = [1, -2, 3, -4, 5, -6, 7, -8, -9, 10];
+// function maxElement(a) {
+//     let sum=a[0]
+//     for (let i = 1; i < a.length; i++) {
+//         if (a[i]>sum) {
+//             let test = a[i]
+//             if (test==a[]) {
+                
+//             }
+//         }
+//     }
+// }
+// let res = maxElement(arr)
+// console.log(res);
+
+///10-masala
+let arr = [1, -2, 3, -4, 5, -6, 7, -8, -9, 10];
+function (params) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
